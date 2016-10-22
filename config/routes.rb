3ddf3f529413df0cbe5c5200cb2d390dc5b7f7cf
Faraz Yashar <faraz.yashar@gmail.com>
2016-10-22 15:14:22 -0400

    Update delete test to use resource route

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :registrations, only: [:create]
-      # resources :records
-      post '/records' => 'records#create'
-      patch '/records' => 'records#update'
-      delete '/records' => 'records#destroy'
+      resources :records
+      # post '/records' => 'records#create'
+      # patch '/records' => 'records#update'
+      # delete '/records' => 'records#destroy'
       post '/update_password' => 'users#update_password'
     end
   end
--- test/api/record_crud_test.rb
+++ test/api/record_crud_test.rb
@@ -2,25 +2,28 @@ require 'test_helper'
 
 class RecordCrudTest < ActionDispatch::IntegrationTest
   test "creates record" do
-  	record_data = {
-  		data: {
-  		  user_id: 2,
-  		  schema: "shape",
-  		  use_case: "survey1",
-  		  record_data: "{'key': 'value'}"
-  		},
-  		headers: {
-  		  content_type: 'application/json'
-  		}
-  	}
-  	post api_v1_records_path, record_data
-  	record = Record.find_by(use_case: record_data[:data][:use_case])
+    record_attrs = {
+      user_id: 2,
+      schema: "shape",
+      use_case: "survey1",
+      record_data: "{'key': 'value'}"
+    }
+    record_data = {
+      data: record_attrs,
+      headers: {
+        content_type: 'application/json'
+      }
+    }
+    post api_v1_records_path, record_data
+
+    last_record = Record.last
+    assert_equal record_attrs, last_record.slice(record_attrs.keys)
     assert_not_nil record
 
-  	expected_response = {
-  		'success' => true,
-  		'data' => {'record_id'=> record.id}
-  	}
+    expected_response = {
+      'success' => true,
+      'data' => {'record_id'=> record.id}
+    }
     assert_response :success
     assert_equal 'application/json', response.content_type
     assert_equal(expected_response, JSON.parse(response.body))
@@ -46,30 +49,39 @@ class RecordCrudTest < ActionDispatch::IntegrationTest
   end
 
   test "deletes record" do
-  	request_data = {
-  		data: {
-  			record_id: 123
-  		},
-  		headers: {
-  			content_type: 'application/json'
-  		}
-  	}
-  	delete api_v1_records_path, request_data
-  	assert_response :success
-  	assert_nil Record.find_by(id: request_data[:data][:record_id])
+    record = Record.find(123)
+    request_data = {
+      headers: {
+        content_type: 'application/json'
+      }
+    }
+    delete api_v1_record_path(record), request_data
+    assert_response :success
+    assert_nil Record.find_by(id: record.id)
+  end
+
+  test "GET /records/:id" do
+  end
+
+  test "GET /records returns all records" do
+
+  end
+
+  test "GET /records?user_case= returns all records for use_case" do
+
   end
 
   # test "reads single record" do
-  # 	request_data = {
-  # 		data: {
-  # 			record_id: 123
-  # 		},
-  # 		headers: {
-  # 			content_type: 'application/json'
-  # 		}
-  # 	}
-  # 	get api_v1_records_path, request_data
-  # 	assert_response :success
-  # 	record = Record.find_by(id: request_data[:record_id])
+  #   request_data = {
+  #     data: {
+  #       record_id: 123
+  #     },
+  #     headers: {
+  #       content_type: 'application/json'
+  #     }
+  #   }
+  #   get api_v1_records_path, request_data
+  #   assert_response :success
+  #   record = Record.find_by(id: request_data[:record_id])
   # end
 end
